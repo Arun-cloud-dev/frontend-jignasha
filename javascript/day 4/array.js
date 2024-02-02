6753fd34c6196ac192d94fd8bbada97d7894e6e3
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-02-02 19:16:40 +0530

    udate

---
+++ javascript/day 4/array.js
@@ -0,0 +1,39 @@
+// var empty = [];
+
+
+// var days = ["sunday", "monday"];
+
+
+
+// var mixed = [fasle, 2.70, "hello "];
+
+
+// var suffix = "tree";
+// var trees = ["oak"+ suffix, "elm"+ suffix];
+// console.log(trees);
+
+
+// var array2 = [[1,{x:1,y:2}],[2,{x:3,y:4}]];
+
+// console.log(array2);
+
+
+// var colors = [ "red","green", undefined]
+
+
+// var hobbies = [,"art"]
+// console.log(hobbies);
+
+
+// var days = ["Sunday", "Monday"]; 
+
+
+
+// var firstday = days[0];
+
+// console.log(firstday);
+
+// days[1]="ford";
+// days [2]= "tuesday"
+
+// console.log(days);
---
+++ javascript/day 4/function.js
@@ -0,0 +1,89 @@
+//syntax
+
+// function nameForfn (p1, p2){
+//     //code to be to exicuted 
+//     return p1 * p2
+// }
+
+//example 
+
+// let x = myFunction (4,3);
+
+
+// function myFunction (a, b){
+//     return a*b
+// }
+
+// console.log(x);
+
+
+// var area = function (radius){
+//     return Math.PI* radius *radius
+// };
+
+
+// console.log(area(5));
+
+
+
+
+// function hoisting 
+// function course (){  //hoisted o top of th eprogram 
+//     return "Learning JS"
+// }
+
+// console.log(course());
+
+
+
+// var num = 1;    //variable s global 
+// function showGlobal(){
+//     console.log(num);  //uses the global num
+// }
+// showGlobal();
+
+// function showLocal (){
+//     var num = 2;    //num is local , hides the global num 
+//     console.log(num);
+// }
+
+// showLocal();
+// showGlobal();
+
+
+
+
+// function showArgumnet(num){
+//     console.log(num);
+// }
+
+
+// showLocal();
+// showGlobal();
+// showArgumnet(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
