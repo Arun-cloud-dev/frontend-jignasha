34c6b44f2f80fea6936ee07a364a39110d483d89
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2024-02-08 08:37:25 +0530

    day 6

---
+++ javascript/day 6 /date.js
@@ -0,0 +1,52 @@
+let currentDate = new Date();
+
+
+// console.log("Current date is:", currentDate);
+
+// let year = currentDate.getFullYear();
+
+// let month = currentDate.getMonth() + 1;
+
+// let day = currentDate.getDate();
+
+
+// console.log("Year:",  year);
+// console.log("Month:", month);
+// console.log("Day", day);
+
+
+
+// currentDate.setFullYear(2005);
+// currentDate.setMonth(0);
+// currentDate.setDate(15);
+
+
+// console.log("updated:", currentDate);
+
+
+// let dayOftheweek = currentDate.getDay("2024-02-02");
+// console.log("day of the week:",dayOftheweek)
+
+
+
+// let formMatedate = currentDate.toISOString();
+
+// console.log("formatted Date:", formMatedate);
+
+
+// const d = new Date("2015");
+
+// console.log(d);
+
+// currentDate.setDate(currentDate.getDate()+5);
+// currentDate.setHours(currentDate.getHours()-3);
+
+
+// console.log("udated date and hours is :",currentDate);
+
+
+
+// let dateString = "2024-08-15";
+
+// let parseDate = new Date(dateString);
+// console.log("parse date from string",parseDate);
